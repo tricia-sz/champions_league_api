a354afb5c43c8dcd30c9edb3757c6fac62cbc4f0
Patricia Souza <patricia.sz.br@gmail.com>
2026-01-05 16:46:05 -0300

    Add express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("HElloMundao");
+import express from 'express'
+import type { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Ola mundo")
+})
+
+
+app.listen(3333)
