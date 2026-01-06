7f783cc1704519b6e93b1640c2707441d01e55d6
Patricia Souza <patricia.sz.br@gmail.com>
2026-01-06 20:32:27 -0300

    Criando a camada service

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,5 +1,8 @@
 import type { Request, Response } from 'express';
+import { getPlayerService } from '../services/players-service.js';
 
-export const getPlayer = (req: Request, res: Response) => {
-    res.status(200).json({player: "Neymar"})
+export const getPlayer = async (req: Request, res: Response) => {
+  const data = await getPlayerService()
+
+    res.status(200).json(data)
 }
---
+++ src/services/players-service.ts
@@ -0,0 +1,3 @@
+export const getPlayerService = () => {
+  return {player: "Neymar"};
+}
